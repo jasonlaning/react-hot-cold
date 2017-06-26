546dea41ef19a412855367a1709ca9771aa07e30
jasonlaning <jason.laning@gmail.com>
2017-06-26 14:56:02 -0400

    reconfigured to use Redux

--- src/components/game.js
+++ src/components/game.js
@@ -1,72 +1,45 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
 import Header from './header';
 import GuessSection from './guess-section';
 import GuessCount  from './guess-count';
 import GuessList from './guess-list';
 
-export default class Game extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            guesses: [],
-            feedback: 'Make your guess!',
-            correctAnswer: Math.floor(Math.random() * 100) + 1,
-        };
-    }
+//import the action(s)
+import {newGame, handleGuess} from '../actions';
+
+import './game.css';
+
+export class Game extends React.Component {
 
     newGame() {
-        this.setState({
-            guesses: [],
-            feedback: 'Make your guess!',
-            correctAnswer: Math.floor(Math.random() * 100) + 1,
-        });
+        this.props.dispatch(newGame());
     }
 
-    guess(guess) {
-        guess = parseInt(guess, 10);
-        if (isNaN(guess)) {
-            this.setState({
-                feedback: 'Please enter a valid number'
-            });
-            return;
-        }
-
-        const difference = Math.abs(guess - this.state.correctAnswer);
-
-        let feedback;
-        if (difference >= 50) {
-            feedback = 'You\'re Ice Cold...';
-        }
-        else if (difference >= 30) {
-            feedback = 'You\'re Cold...';
-        }
-        else if (difference >= 10) {
-            feedback = 'You\'re Warm';
-        }
-        else if (difference >= 1) {
-            feedback = 'You\'re Hot!';
-        }
-        else {
-            feedback = 'You got it!';
-        }
-
-        this.setState({
-            feedback,
-            guesses: [...this.state.guesses, guess]
-        });
+    handleGuess(guess) {
+        this.props.dispatch(handleGuess(guess));
     }
 
     render() {
         return (
             <div>
                 <Header onNewGame={() => this.newGame()}/>
-                <GuessSection feedback={this.state.feedback}
-                    onGuess={(guess) => this.guess(guess)} />
-                <GuessCount count={this.state.guesses.length} />
-                <GuessList guesses={this.state.guesses} />
+                <div className="game-container">
+                    <GuessSection feedback={this.props.feedback}
+                        onGuess={(guess) => this.handleGuess(guess)} />
+                    <GuessCount count={this.props.guesses.length} />
+                    <GuessList guesses={this.props.guesses} />
+                </div>
             </div>
         );
     }
 }
 
+const mapStateToProps = state => ({
+            guesses: state.guesses,
+            feedback: state.feedback,
+            correctAnswer: state.correctAnswer
+});
+
+export default connect(mapStateToProps)(Game);
